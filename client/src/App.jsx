3a7c92645d264b9e958014ea57a912c7a2a62b6f
Cyril Ayegbe <cma595@usask.ca>
2024-02-05 18:08:36 -0600

    created routes for various pages

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,5 +1,34 @@
+import { createBrowserRouter, RouterProvider, Outlet } from "react-router-dom";
+
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Layout />,
+    children: [
+      {
+        path: "/",
+        element: <Home />,
+      },
+      {
+        path: "/products/:id",
+        element: <Products />,
+      },
+      {
+        path: "/product/:id",
+        element: <Product />,
+      },
+    ],
+  },
+]);
+
+
 function App() {
-  return <div>Hello World!</div>;
+  return 
+  <div>
+    Hello World!
+    <RouterProvider router={router}/>
+  </div>;
 }
 
 export default App;
